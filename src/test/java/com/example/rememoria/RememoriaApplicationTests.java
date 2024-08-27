cb08935f5634c3ab42497f4287660cc2e8af6c7a
moovingsloth <dlehddnjs245@khu.ac.kr>
2024-08-27 22:51:03 +0900

    CI 추가

---
+++ src/test/java/com/example/rememoria/RememoriaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rememoria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RememoriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
