90d7b2c03cf41f6cdedb67cc225cccdd76e0dfd2
moovingsloth <dlehddnjs245@khu.ac.kr>
2024-09-05 01:32:01 +0900

    Save current changes

---
+++ client/src/App.tsx
@@ -0,0 +1,15 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { lazy, Suspense, useEffect } from 'react';
+
+function App() {
+  return (
+    <BrowserRouter>
+    <Routes>
+      <Route />
+      <Route />
+    </Routes>
+  </BrowserRouter>
+);
+}
+
+export default App;
