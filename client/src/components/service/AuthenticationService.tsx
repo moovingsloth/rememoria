cd63c821c5ae57d0cdad414645d0edaeb05be934
moovingsloth <dlehddnjs245@khu.ac.kr>
2024-09-10 18:21:47 +0900

    Kakao login 오류 수정

--- client/src/components/service/AuthenticationService.tsx
+++ client/src/components/service/AuthenticationService.tsx
@@ -5,7 +5,7 @@ const BACK_END_URL = process.env.REACT_APP_BACK_END_URL;
 export const kakaoLogin = async (code: string) => {
     console.log('kakaoLogin called with code:', code); // 추가 로그
     try {
-        const response = await axios.get(`${BACK_END_URL}/login/oauth/kakao?code=${code}`);
+        const response = await axios.get(`${BACK_END_URL}?code=${code}`);
         return response.data;
     } catch (error) {
         console.error('Error during Kakao login', error);
